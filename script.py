27755b31ce919c5ece30824b1cc1d5e5c1d74a3e
50shadesofgwei <111451828+50shadesofgwei@users.noreply.github.com>
2023-06-03 10:51:02 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,4 @@
 
-
 import requests 
 import json
 import time
@@ -8,201 +7,201 @@ import hashlib
 import numpy as np
 import http.client
 from binance.client import Client
+from requests.exceptions import RequestException
+from binance.exceptions import BinanceAPIException, BinanceOrderException
 
+# All keys are for testing wallets only and have no access to capital
+# All keys are for testing wallets only and have no access to capital
+# All keys are for testing wallets only and have no access to capital
 
-# NOTE: All keys are for testing wallets and have no access to any capital
-# NOTE: All keys are for testing wallets and have no access to any capital
-# NOTE: All keys are for testing wallets and have no access to any capital
-
-# Binance keys
-client = Client(
-    'EplcH4b00prUTAN4PRs2cAHO88kYGztwjZn7Ezi1BjrfNHItvidHKIBPebPRMmCw',
-    'umg6kwhTo2le1RNYWridgkSmy7e2R7RN904HTBc4cex1XMULQnXWPdsGXVFf3PVe'
-    )
-
-# Coinbase sig + keys
-secretKey = "YhgPlByvWIGeNUP4lgkumniGao7usjha"
-accessKey = "DFtojCm1QzzWWCXp"
-
-timestamp = str(int(time.time()))
-method = "GET"
-url_path = "/api/v3/brokerage/accounts"
-url = "https://api.coinbase.com/api/v3/brokerage/accounts"
-body = ""
-message = timestamp + method + url_path + body
-
-signature = hmac.new(secretKey.encode(
-    "utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256).digest()
-
-coinbaseHeaders = {
-    "CB-ACCESS-KEY": accessKey,
-    "CB-ACCESS-SIGN": signature.hex(),
-    "CB-ACCESS-TIMESTAMP": timestamp,
-}
-
-# Store price request URLs for CEXes
-binanceAPI = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
-coinbaseAPI = requests.get('https://api.coinbase.com/v2/prices/BTC-USD/buy', headers=coinbaseHeaders)
-
-# Get amount held in accounts
-biHoldings = client.get_asset_balance(asset='BTC')
-coinHoldingsRaw = requests.get("https://api.coinbase.com/api/v3/brokerage/accounts", headers = coinbaseHeaders)
-coinHoldings = coinHoldingsRaw.json()
-
-# Parse price data, store as float 
-binanceAPI = binanceAPI.json()
-bistring = str(binanceAPI)
-bistring = bistring[32:]
-biFilString = ''.join(char for char in bistring if char.isdigit() or char == '.')
-biFloat = float(biFilString)
-
-
-coinbaseAPI = coinbaseAPI.json()
-coinstring = str(coinbaseAPI)
-coinstring = coinstring[55:]
-filtered_string = ''.join(char for char in coinstring if char.isdigit() or char == '.')
-coinFloat = float(filtered_string)
-
-# Parse account holdings, store as float
-biHoldingsStr = str(biHoldings)
-biHoldingsStr = biHoldingsStr[22:50]
-biHoldingsStr = ''.join(char for char in biHoldingsStr if char.isdigit() or char == '.')
-biAccFloat = float(biHoldingsStr)
-
-coinHoldings = str(coinHoldings)
-coinHoldings = coinHoldings[130:157]
-coinHoldings = ''.join(char for char in coinHoldings if char.isdigit() or char == '.')
-coinAccFloat = float(coinHoldings)
-
-# Basic math to check if arb is profitable
-# Define trade amount
-TradeAmount = 1000
-
-# Assume zero fees
-
-def checkProfitability1(biFloat, coinFloat):
-
-    path1 = coinFloat - biFloat
-
-    # Returns true if price is higher on Coinbase
-    if path1 > 0:
-        return True
-    
-    else: return False
-    
-
-    
-    
-
-def checkProfitability2(biFloat, coinFloat):
-
-    path2 = biFloat - coinFloat
-
-    # Returns true if price is higher on Binance
-    if path2 > 0:
-        return True
-
-    else: return False
-
-
-
-path1bool = checkProfitability1(biFloat, coinFloat)
-path2bool = checkProfitability2(biFloat, coinFloat)
-
-# If path1bool == True then send a sell order to Coinbase and a buy order to Binance
-
-if path1bool == True:
-
-    # Coinbase sell order
-    conn = http.client.HTTPSConnection("api.coinbase.com")
-    method = "POST"
-    url_path = "/api/v3/brokerage/orders"
-    int_order_id = np.random.randint(2**63)
+# All coinbase vars/defs
+class Coinbase:
+    BASE_URL = "https://api.coinbase.com"
+    secretKey = "YhgPlByvWIGeNUP4lgkumniGao7usjha"
+    accessKey = "DFtojCm1QzzWWCXp"
     timestamp = str(int(time.time()))
-    url = "https://api.coinbase.com/api/v3/brokerage/orders"
-    body = ""
 
+    def __init__(self, secretKey, accessKey):
+        self.secretKey = secretKey
+        self.accessKey = accessKey
+
+    def get_signature(self, timestamp, method, url_path, body):
+        message = timestamp + method + url_path + body
+        signature = hmac.new(self.secretKey.encode(
+          "utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256).digest()
+        return signature
+
+    def get_headers(self, method, url_path, body):
+        signature = self.get_signature(self.timestamp, method, url_path, body)
+        headers = {
+            "CB-ACCESS-KEY": self.accessKey,
+            "CB-ACCESS-SIGN": signature.hex(),
+            "CB-ACCESS-TIMESTAMP": self.timestamp,
+        }
+        return headers
+
+    def get_btc_usd_price(self):
+        method = "GET"
+        url_path = "/v2/prices/BTC-USD/buy"
+        headers = self.get_headers(method, url_path)
+        try:
+            response = requests.get(self.BASE_URL + url_path, headers=headers)
+        except Exception as err:
+            print(f"An error occurred when calling price from Coinbase: {err}")
+            return None
+        else:
+          coinbaseAPI = response.json()
+          coinstring = str(coinbaseAPI)
+          coinstring = coinstring[55:]
+          filtered_string = ''.join(char for char in coinstring if char.isdigit() or char == '.')
+          coinFloat = float(filtered_string)  
+          return coinFloat
     
-    baseSize = 1/(coinFloat/1000)
-    baseSize = str(baseSize)
-    payload = json.dumps({
-        "client_order_id": str(int_order_id),
-        "product_id": "BTC-USD",
-        "side": "SELL",
-        "order_configuration": {
-        "market_market_ioc": {
-        "quote_size": "1000"
+    def buy_order(self):
+        conn = http.client.HTTPSConnection("api.coinbase.com")
+        method = "POST"
+        url_path = "/api/v3/brokerage/orders"
+        int_order_id = np.random.randint(2**62)
+        str_order_id = str(int_order_id)
+        payload = json.dumps({
+            "client_order_id": str_order_id,
+            "product_id": "BTC-USD",
+            "side": "BUY",
+            "order_configuration": {
+            "market_market_ioc": {
+            "quote_size": "1000"
+        }
+        }
+    })
+        headers = self.get_headers()
+        try:
+            conn.request(method, url_path, payload, headers=headers)
+            res = conn.getresponse()
+            data = res.read()
+            if res.status != 200:
+                print(f"Request failed with status {res.status}: {data.decode('utf-8')}")
+            else:
+                print(data.decode("utf-8"))
+        except http.client.HTTPException as err:
+            print(f"A HTTP error occurred: {err}")
+        except Exception as err:
+            print(f"An error occurred: {err}")
+
+    def sell_order(self):
+        conn = http.client.HTTPSConnection("api.coinbase.com")
+        method = "POST"
+        url_path = "/api/v3/brokerage/orders"
+        int_order_id = np.random.randint(2**63)
+        coinFloat = self.get_btc_usd_price(self)
+        baseSize = 1/(coinFloat/1000)
+        baseSize = str(baseSize)
+        payload = json.dumps({
+            "client_order_id": str(int_order_id),
+            "product_id": "BTC-USD",
+            "side": "SELL",
+            "order_configuration": {
+            "market_market_ioc": {
+            "base_size": baseSize
+        }
     }
-  }
-})
-    message = timestamp + method + url_path + str(payload)
-    signature = hmac.new(secretKey.encode("utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256).digest()
+    })
+        headers = self.get_headers()
+        try:
+            conn.request(method, url_path, payload, headers=headers)
+            res = conn.getresponse()
+            data = res.read()
+            if res.status != 200:
+                print(f"Request failed with status {res.status}: {data.decode('utf-8')}")
+            else:
+                print(data.decode("utf-8"))
+        except http.client.HTTPException as err:
+            print(f"A HTTP error occurred with the Coinbase client: {err}")
+        except Exception as err:
+            print(f"An error occurred with the Coinbase client: {err}")
+        
 
-    headers={
-    'CB-ACCESS-KEY': accessKey,
-    'CB-ACCESS-TIMESTAMP': timestamp,
-    'CB-ACCESS-SIGN': signature.hex()
-    }
+# All binance vars/defs
+class Binance:
+    secretKey = 'EplcH4b00prUTAN4PRs2cAHO88kYGztwjZn7Ezi1BjrfNHItvidHKIBPebPRMmCw'
+    accessKey = 'umg6kwhTo2le1RNYWridgkSmy7e2R7RN904HTBc4cex1XMULQnXWPdsGXVFf3PVe'
+    client = Client(secretKey, accessKey)
 
-    conn.request(method, url_path, payload, headers=headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
-
-    # Binance buy order
-    order = client.create_order(
-        symbol = 'BTCUSDT',
-        side = Client.SIDE_BUY,
-        type = Client.ORDER_TYPE_MARKET,
-        quoteOrderQty = 1000
-    )
-
-# If path2bool == True, send a buy order to Coinbase and a sell order to Binance
-if path2bool == True:
-
-    conn = http.client.HTTPSConnection("api.coinbase.com")
-    method = "POST"
-    url_path = "/api/v3/brokerage/orders"
-    int_order_id = np.random.randint(2**62)
-    str_order_id = str(int_order_id)
-    timestamp = str(int(time.time()))
-    url = "https://api.coinbase.com/api/v3/brokerage/orders"
-    body = ""
-    message = timestamp + method + url_path + body
-
-    signature = hmac.new(secretKey.encode(
-    "utf-8"), message.encode("utf-8"), digestmod=hashlib.sha256).digest()
-    baseSize = 1/(coinFloat/1000)
-    baseSize = str(baseSize)
-    payload = json.dumps({
-        "client_order_id": str_order_id,
-        "product_id": "BTC-USD",
-        "side": "BUY",
-        "order_configuration": {
-        "market_market_ioc": {
-        "quote_size": "1000"
-    }
-  }
-})
-    message = timestamp + method + url_path.split('?')[0] + str(payload)
-
-    headers={
-    'CB-ACCESS-KEY': accessKey,
-    'CB-ACCESS-TIMESTAMP': timestamp,
-    'CB-ACCESS-SIGN': signature.hex(),
-    'accept':'application/json'
-    }
-
+    def __init__(self):
+        pass
     
-    conn.request(method, url_path, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    print(data.decode("utf-8"))
+    def get_BTCUSDT_price(self):
+        try:
+            binancePrice = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT')
+        except Exception as err:
+            print(f"An error occurred when calling price from Binance: {err}")
+            return None
+        binancePrice = binancePrice.json()
+        bistring = str(binancePrice)
+        bistring = bistring[32:]
+        biFilString = ''.join(char for char in bistring if char.isdigit() or char == '.')
+        biFloat = float(biFilString)
+        return biFloat
 
-    # Binance sell order
-    order = client.create_order(
-        symbol = 'BTCUSDT',
-        side = Client.SIDE_SELL,
-        type = Client.ORDER_TYPE_MARKET,
-        quoteOrderQty = 1000
-    )
+    def buy_order(self):
+      try:
+          self.order = self.client.create_order(
+              symbol='BTCUSDT',
+              side=self.client.SIDE_BUY,
+              type=self.client.ORDER_TYPE_MARKET,
+              quoteOrderQty=1000
+          )
+      except BinanceAPIException as e:
+        print(f"Binance API returned an error. Error code: {e.status_code}, Error message: {e.message}")
+      except BinanceOrderException as e:
+        print(f"Order failed. Error message: {e.message}")
+      except Exception as e:
+        print(f"An unknown error occurred. Error message: {e}")
+
+    def sell_order(self):
+      try:
+          self.order = self.client.create_order(
+            symbol = 'BTCUSDT',
+            side = self.client.SIDE_SELL,
+            type = self.client.ORDER_TYPE_MARKET,
+            quoteOrderQty = 1000
+          )
+      except BinanceAPIException as e:
+        print(f"Binance API returned an error. Error code: {e.status_code}, Error message: {e.message}")
+      except BinanceOrderException as e:
+        print(f"Order failed. Error message: {e.message}")
+      except Exception as e:
+        print(f"An unknown error occurred. Error message: {e}")
+    
+
+class Calculations:
+    def __init__(self):
+        self.coinbaseInstance = Coinbase()
+        self.binanceInstance = Binance()
+        
+
+    def callCoinPrice(self):
+        coinPrice = self.coinbaseInstance.get_btc_usd_price()
+        return coinPrice
+    
+    def callBiPrice(self):
+        biPrice = self.binanceInstance.get_BTCUSDT_price()
+        return biPrice
+
+    def checkProfitability1(self, biPrice, coinPrice):
+        biPrice = self.callBiPrice()
+        coinPrice = self.callCoinPrice()
+        path1 = biPrice - coinPrice
+        # Returns true if price is higher on Binance
+        if path1 > 0:
+            return True
+        else: return False
+
+    def checkProfitability2(self, biPrice, coinPrice):
+        biPrice = self.callBiPrice()
+        coinPrice = self.callCoinPrice()
+        path2 = coinPrice - biPrice
+        # Returns true if price is higher on Coinbase
+        if path2 > 0:
+            return True
+        else: return False
+
