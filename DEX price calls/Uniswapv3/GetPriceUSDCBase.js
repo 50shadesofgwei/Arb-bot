43cc45ada5b7e93fa664f0fefb47af69d810c0f9
50shadesofgwei <111451828+50shadesofgwei@users.noreply.github.com>
2023-07-03 10:49:04 -0500

    Update and rename GetPrice.js to GetPriceUSDCBase.js

--- DEX price calls/Uniswapv3/GetPriceUSDCBase.js
+++ DEX price calls/Uniswapv3/GetPriceUSDCBase.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 const { ethers } = require("ethers");
 const { abi: IUniswapV3PoolABI } = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
 const { abi: QuoterABI } = require("@uniswap/v3-periphery/artifacts/contracts/lens/Quoter.sol/Quoter.json");
@@ -10,11 +14,9 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_URL)
 
-const poolAddress = '0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640'
-
 const quoterAddress = "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6"
 
-const getPrice = async (inputAmount) => {
+const getPrice = async (inputAmount, poolAddress) => {
   const poolContract = new ethers.Contract(
     poolAddress,
     IUniswapV3PoolABI,
@@ -38,9 +40,9 @@ const getPrice = async (inputAmount) => {
     provider
   )
 
-  const tokenSymbol0 = await tokenContract0.symbol()
+  const tokenSymbol0 = 'USDC'
   const tokenSymbol1 = await tokenContract1.symbol()
-  const tokenDecimals0 = await tokenContract0.decimals()
+  const tokenDecimals0 = 6
   const tokenDecimals1 = await tokenContract1.decimals()
 
   const quoterContract = new ethers.Contract(
@@ -53,10 +55,10 @@ const getPrice = async (inputAmount) => {
 
   const amountIn = ethers.utils.parseUnits(
     inputAmount.toString(),
-    tokenDecimals0
+    tokenDecimals1
   )
 
-  const quotedAmountOut = await quoterContract.callStatic.quoteExactInputSingle(
+  const quotedAmountOut = await quoterContract.callStatic.quoteExactOutputSingle(
     immutables.token0,
     immutables.token1,
     immutables.fee,
@@ -64,11 +66,24 @@ const getPrice = async (inputAmount) => {
     0
   )
 
-  const amountOut = ethers.utils.formatUnits(quotedAmountOut, tokenDecimals1)
+  const amountOut = ethers.utils.formatUnits(quotedAmountOut, tokenDecimals0)
 
-  console.log('=========')
-  console.log(`${inputAmount} ${tokenSymbol0} can be swapped for ${amountOut} ${tokenSymbol1}`)
-  console.log('=========')
+  
+  console.log(`${inputAmount} ${tokenSymbol1} can be swapped for ${amountOut} ${tokenSymbol0}`)
+
+  return amountOut
 }
 
-getPrice(1)
+app.get('/getPrice', async (req, res) => {
+  // Get parameters from the request
+  const amount = req.query.amount;
+  const poolAddress = req.query.poolAddress;
+  
+  // call getPrice function
+  const price = await getPrice(amount, poolAddress);
+  res.send({ price });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
